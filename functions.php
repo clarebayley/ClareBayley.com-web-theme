14cd71c77f0c196584d26a0147ca25a6729f781c
Clare Bayley <clareb@hattery.com>
2013-07-30 23:19:21 -0700

    fixing rss nsfw… again.
    sigh. hopefully this is the last time.

--- functions.php
+++ functions.php
@@ -1,40 +1,4 @@
 <?php
-	// enables wigitized sidebars
-	if ( function_exists('register_sidebar') )
-
-	// Sidebar Widget
-	// Location: the sidebar
-	register_sidebar(array('name'=>'Sidebar',
-		'before_widget' => '<div class="widget-area widget-sidebar"><ul>',
-		'after_widget' => '</ul></div>',
-		'before_title' => '<h3>',
-		'after_title' => '</h3>',
-	));
-	// Header Widget
-	// Location: right after the navigation
-	register_sidebar(array('name'=>'Header',
-		'before_widget' => '<div class="widget-area widget-header"><ul>',
-		'after_widget' => '</ul></div>',
-		'before_title' => '<h4>',
-		'after_title' => '</h4>',
-	));
-	// Footer Widget
-	// Location: at the top of the footer, above the copyright
-	register_sidebar(array('name'=>'Footer',
-		'before_widget' => '<div class="widget-area widget-footer"><ul>',
-		'after_widget' => '</ul></div>',
-		'before_title' => '<h4>',
-		'after_title' => '</h4>',
-	));
-	// The Alert Widget
-	// Location: displayed on the top of the home page, right after the header, right before the loop, within the content area
-	register_sidebar(array('name'=>'Alert',
-		'before_widget' => '<div class="widget-area widget-alert"><ul>',
-		'after_widget' => '</ul></div>',
-		'before_title' => '<h4>',
-		'after_title' => '</h4>',
-	));
-
 	// post thumbnail support
 	add_theme_support( 'post-thumbnails' );
 	// adds the post thumbnail to the RSS feed
@@ -46,44 +10,9 @@
 	    }
 	    return $content;
 	}
-	add_filter('the_excerpt_rss', 'cwc_rss_post_thumbnail');
-	add_filter('the_content_feed', 'cwc_rss_post_thumbnail');
+	//add_filter('the_excerpt_rss', 'cwc_rss_post_thumbnail');
+	//add_filter('the_content_feed', 'cwc_rss_post_thumbnail');
 
-	// custom menu support
-	add_theme_support( 'menus' );
-	if ( function_exists( 'register_nav_menus' ) ) {
-	  	register_nav_menus(
-	  		array(
-	  		  'header-menu' => 'Header Menu',
-	  		  'sidebar-menu' => 'Sidebar Menu',
-	  		  'footer-menu' => 'Footer Menu',
-	  		  'logged-in-menu' => 'Logged In Menu'
-	  		)
-	  	);
-	}
-
-	// custom background support
-	add_custom_background();
-
-	// custom header image support
-	define('NO_HEADER_TEXT', true );
-	define('HEADER_IMAGE', '%s/images/default-header.png'); // %s is the template dir uri
-	define('HEADER_IMAGE_WIDTH', 1068); // use width and height appropriate for your theme
-	define('HEADER_IMAGE_HEIGHT', 300);
-	// gets included in the admin header
-	function admin_header_style() {
-	    ?><style type="text/css">
-	        #headimg {
-	            width: <?php echo HEADER_IMAGE_WIDTH; ?>px;
-	            height: <?php echo HEADER_IMAGE_HEIGHT; ?>px;
-	        }
-	    </style><?php
-	}
-	add_custom_image_header( '', 'admin_header_style' );
-
-	// adds Post Format support
-	// learn more: http://codex.wordpress.org/Post_Formats
-	// add_theme_support( 'post-formats', array( 'aside', 'gallery','link','image','quote','status','video','audio','chat' ) );
 
 	// removes detailed login error information for security
 	add_filter('login_errors',create_function('$a', "return null;"));
@@ -106,17 +35,6 @@
 			return $count;
 		}
 	}
-
-	// invite rss subscribers to comment
-	function rss_comment_footer($content) {
-		if (is_feed()) {
-			if (comments_open()) {
-				$content .= 'Comments are open! <a href="'.get_permalink().'">Add yours!</a>';
-			}
-		}
-		return $content;
-	}
-
 	// custom excerpt ellipses for 2.9+
 	function custom_excerpt_more($more) {
 		return 'Read More &raquo;';
@@ -159,49 +77,7 @@
 	// 	add_theme_page( __( 'Theme Options', 'tat_theme' ), __( 'Theme Options', 'tat_theme' ), 'edit_theme_options', 'theme_options', 'theme_options_do_page' );
 	// }
 	
-	// begin LifeGuard Assistant
-	// learn more about the LifeGuard Assistant: http://wplifeguard.com/lifeguard-plugin/
-	// learn more about the affiliate program: http://wplifeguard.com/affiliates/
-	add_action('admin_menu', 'lgap_add_pages');
-	function lgap_add_pages() {
-		add_menu_page(__('Help','menu-test'), __('Help','menu-test'), 'read', 'lifeguard-assistant-plugin', 'lgap_main_page' );
-	}
-	function lgap_main_page() {
-		echo "<h2>" . __( 'Help', 'menu-test' ) . "</h2>";
-		// place your affiliate ID between the " on the following line
-		$lgap_aff = "";
-		// get your affiliate ID here: http://wplifeguard.com/wp-admin/profile.php?page=affiliateearnings
-		echo '
-		<style type="text/css">
-			#wplg { font-family: "Varela",Helvetica,Trebuchet MS,Verdana,"DejaVu Sans",sans-serif; }
-			#wplg a:link,#wplg a:visited { color: #21759b; text-decoration: none; }
-			#wplg a:hover { color: #d54e21; }
-			.wplg-video { background: #f6f6f6; border: 1px solid #dadada; padding: 12px; margin: 0 12px 12px 0; float: left; }
-			.wplg-clear { clear: both; }
-			.wplg-green-button { box-shadow:inset 0 0 3px rgba(0,0,0,.1); font-size: 20px; line-height: 32px; height: 32px; width: 434px; margin: 0 12px 12px 0; text-align: center; display: block; border: 2px solid #9abf89; background: #7da742; color: #f1ffeb !important; text-shadow: 0 0 3px rgba(125,167,66,.75); }
-			.wplg-green-button:hover { border: 2px solid #c0e1aa; background: #8ac636; }
-			.wplg-green-button:active { border: 2px solid #88a65e; background: #5d822a; }
-		</style>
-		<link href="http://fonts.googleapis.com/css?family=Varela" rel="stylesheet" type="text/css">
-		<div id="wplg">
-			<p>Need help with WordPress? Here is a collection of free WordPress video tutorials from <a href="http://wplifeguard.com/'.$lgap_aff.'">wpLifeGuard</a> to help you get going. <a href="http://wplifeguard.com/get-access/'.$lgap_aff.'">Get access to more videos.</a></p>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32852753?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32856785?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32857648?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32860297?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32872861?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32878118?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32881530?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32864178?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32863614?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32862744?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-video"><iframe src="http://player.vimeo.com/video/32857481?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			<div class="wplg-clear"></div>
-			<a class="wplg-green-button" href="http://wplifeguard.com/get-access/'.$lgap_aff.'">Get Full Access Now</a>
-		</div>
-		';
-	}
-	// end LifeGuard Assistant
+
 	
 	function twentyeleven_comment( $comment, $args, $depth ) {
 		$GLOBALS['comment'] = $comment;
@@ -268,4 +144,18 @@ function strip_nsfw( $content ) {
 
 add_filter('strip_nsfw','strip_nsfw');
 	
+function the_content_feed_sfw( $content ) {
+	$content = apply_filters('strip_nsfw',$content);
+	return $content;
+}	
+
+add_filter('the_content_feed', 'the_content_feed_sfw');
+
+function the_excerpt_rss_sfw( $output ) {
+	$output = apply_filters('strip_nsfw',$output);
+	return $output;
+}
+
+add_filter('the_excerpt_rss', 'the_excerpt_rss_sfw');
+
 ?>
